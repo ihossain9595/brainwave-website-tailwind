c05da25f113a72942416d7a0c515e26765a2c81a
Ismail Hossain <ismail.9595@gmail.com>
2024-06-19 19:09:11 +0600

    finish roadmap section

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -40,7 +40,7 @@ const Collaboration = () => {
 
             <ul>
               {collabApps.map((app, index) => (
-                <li className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${index * 45}`}>
+                <li className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${index * 45}`} key={index}>
                   <div className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${index * 45}`}>
                     <img src={app.icon} alt={app.title} width={app.width} height={app.height} className="m-auto" />
                   </div>
--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -6,7 +6,7 @@ const PricingList = () => {
   return (
     <div className="flex gap-[1rem] max-lg:flex-wrap">
       {pricing.map((item) => (
-        <div className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3">
+        <div className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[2rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3" key={item.id}>
           <h4 className="h4 mb-4">{item.title}</h4>
           <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">{item.description}</p>
 
